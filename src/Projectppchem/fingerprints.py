a2c71f307cff18ebee5dbee59325d4a084e7ab7b
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-12 16:20:00 +0200

    Update fingerprints.py

--- src/Projectppchem/fingerprints.py
+++ src/Projectppchem/fingerprints.py
@@ -1,4 +1,54 @@
-#Import all the package
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Descriptors, MoleculeDescriptors
+from tqdm import tqdm
+from lightgbm import LGBMRegressor
+import pickle
 
-## def toutes les fonction ici
+def process_csv(file_path):
+    data = pd.read_csv(file_path)
+    smiles = data.iloc[:, 0].tolist()  # Assuming SMILES in the first column
+    return smiles
 
+def canonical_SMILES(smiles):
+    canon_smls = [Chem.CanonSmiles(smls) for smls in smiles]
+    return canon_smls
+
+def RDkit_descriptors(smiles):
+    mols = [Chem.MolFromSmiles(i) for i in smiles]
+    calc = MoleculeDescriptors.MolecularDescriptorCalculator([x[0] for x in Descriptors._descList])
+    desc_names = calc.GetDescriptorNames()
+
+    Mol_descriptors = []
+    for mol in tqdm(mols):
+        mol = Chem.AddHs(mol)
+        descriptors = calc.CalcDescriptors(mol)
+        Mol_descriptors.append(descriptors)
+    return Mol_descriptors, desc_names
+
+def load_model_and_scalers(model_path='model_LGBM.pkl', scaler_path='model_scaler.pkl'):
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
+
+    with open(scaler_path, 'rb') as scaler_file:
+        scaler = pickle.load(scaler_file)
+
+    return model, scaler
+
+def predict_LogS(smiles):
+    canonical_smiles = canonical_SMILES([smiles])
+    descriptors, _ = RDkit_descriptors(canonical_smiles)
+
+    model, scaler = load_model_and_scalers()
+    scaled_descriptors = scaler.transform(descriptors)
+
+    logS_prediction = model.predict(scaled_descriptors)
+    return logS_prediction[0]
+
+def get_logS_str():
+    smiles_code = input("Enter a SMILES code: ")
+    logS = predict_LogS(smiles_code)
+    print(f"Predicted LogS value for {smiles_code}: {logS}")
+
+# Example usage:
+get_logS_str()
