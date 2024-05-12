84f9a75185d6d39f719b89086b838c828b5110c4
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-12 17:01:43 +0200

    Update fingerprints.py

--- src/Projectppchem/fingerprints.py
+++ src/Projectppchem/fingerprints.py
@@ -54,18 +54,6 @@ def predict_LogS(smiles):
     return logS_prediction[0]
 
 def get_logS_str(smiles_code):
-    print(
-         "\
-     ***********************************
-     *    _______          _______     *
-     *    / ____\ \        / /  __ \   *
-     *   | (___  \ \  /\  / /| |__) |  *
-     *    \___ \  \ \/  \/ / |  ___/   *
-     *    ____) |  \  /\  /  | |       *
-     *   |_____/    \/  \/   |_|       *
-     *                                 *
-     ***********************************"
-         )
     #smiles_code = input("Please enter a SMILES code :")
     logS = predict_LogS(smiles_code)
     print(f"Predicted LogS value for {smiles_code}: {logS}")
