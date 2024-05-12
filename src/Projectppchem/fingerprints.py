8bbfa2128267af0791eda21f218b88ace8fec7fb
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-12 17:10:50 +0200

    Update fingerprints.py

--- src/Projectppchem/fingerprints.py
+++ src/Projectppchem/fingerprints.py
@@ -53,16 +53,17 @@ def predict_LogS(smiles):
     logS_prediction = model.predict(scaled_descriptors)
     return logS_prediction[0]
     
-def get_logS_str(smiles_code):
-    if not Chem.MolFromSmiles(smiles_code):
-        print("Invalid SMILES code. Please enter a valid SMILES.")
-    else:
-        try:
-            float(smiles_code)  # Check if input is a float
-            print("Invalid input. Please enter a SMILES code, not a float value.")
-        except ValueError:
-            logS = predict_LogS(smiles_code)
-            print(f"Predicted LogS value for {smiles_code}: {logS}")
+def get_logS_str(*smiles_code):
+    for smiles_code in smiles_codes:
+        if not Chem.MolFromSmiles(smiles_code):
+            print(f"Invalid SMILES code: {smiles_code}. Skipping.")
+        else:
+            try:
+                float(smiles_code)  # Check if input is a float
+                print(f"Invalid input: {smiles_code}. Skipping.")
+            except ValueError:
+                logS = predict_LogS(smiles_code)
+                print(f"Predicted LogS value for {smiles_code}: {logS}")
 
 # Example usage:
 # get_logS_str("SMILE")
