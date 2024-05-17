6a72ccd244f35d81206e578100a3059f3aca9ce7
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-17 08:32:24 +0200

    Update setup.py

--- src/WSPP/setup.py
+++ src/WSPP/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="WSPP",
     version="0.1",
-    author="Cossard Lucas and Venancio Enzo"
+    author="Cossard Lucas and Venancio Enzo",
     packages=find_packages(),
     install_requires=[
         "python=3.8",
