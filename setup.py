7430776628786dbdcc17f479f4b9f7a7830412a1
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-17 09:24:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author="Cossard Lucas and Venancio Enzo",
     packages=find_packages(),
-    description = active learning to accelerate virtual drug discovery,
+    description = "water solubility prediction project",
     long_description = file: README.md,
     url="https://github.com/Nohalyan/WSPP_Projectppchem",
     python_requires='>=3.8', 
