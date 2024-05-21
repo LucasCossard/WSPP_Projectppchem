6d43fcccc0c5d1aafc3d879382942491133b09e0
Nohalyan <160868437+Nohalyan@users.noreply.github.com>
2024-05-21 11:25:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="wsppchem",
-    version="0.8",
+    version="0.9",
     author="Cossard Lucas and Enzo Venancio",
     author_email="lucas.cossard@epfl.ch and enzo.venancio@epfl.ch",
     description="Water Solubility Prediction Project",
